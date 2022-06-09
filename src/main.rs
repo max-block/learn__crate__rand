64f4d5a2e143b932bb1ab3652b70be67aff24d14
Max Block <maxblock@pm.me>
2022-06-09 11:23:32 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use rand::Rng;
+use rand::{Rng, prelude::SliceRandom};
 
 fn main() {
     let mut rng = rand::thread_rng();
 
     // random from range
     dbg!(rng.gen_range(17..=23));
+
+    // random from vec
+    let vec_data = vec!["a", "b", "c"];
+    dbg!(vec_data.choose(&mut rng)); // SliceRandom
 }
